529db0a73ec0b5b58d78efe674e39e438952250b
ciestellato <96916847+ciestellato@users.noreply.github.com>
2025-10-08 10:37:35 +0900

    モデルを定義してテーブルを作成

--- django_bat_sample/django_web/calculator/models.py
+++ django_bat_sample/django_web/calculator/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# 話題テーブルの定義
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        "モデルの文字列表現を返す"
+        return self.text
