0b7a9840b49b4564b1edd847405a260df40a94a3
ciestellato <96916847+ciestellato@users.noreply.github.com>
2025-10-08 15:35:52 +0900

    入力欄を追加

--- django_bat_sample/django_web/calculator/views.py
+++ django_bat_sample/django_web/calculator/views.py
@@ -11,12 +11,14 @@ def index(request):
 	return render(request, 'calculator/index.html', {'topics': topics})
 
 def bmi_view(request):  # BMI計算ページのビュー関数
-    bmi = None  # 初期状態ではBMIは未計算
+    bmi = None  	# 初期状態ではBMIは未計算
+    nick_name = ""	# 初期状態では空文字
     if request.method == 'POST':  # フォームが送信されたとき
+        nick_name = request.POST['nick_name']	# 名前を取得
         height = float(request.POST['height'])  # 身長を取得（cm）
         weight = float(request.POST['weight'])  # 体重を取得（kg）
         height_m = height / 100  # cm → m に変換
         bmi = round(weight / (height_m ** 2), 2)  # BMI計算（小数第2位まで）
 
     # GET,POSTともにbmi.htmlを表示
-    return render(request, 'calculator/bmi.html', {'bmi': bmi})  # テンプレートにBMIを渡す
+    return render(request, 'calculator/bmi.html', {'bmi': bmi, 'nick_name': nick_name})  # テンプレートにBMIを渡す
