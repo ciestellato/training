cd98b52a88d171e4efe17ca2e29987b03d932309
ciestellato <96916847+ciestellato@users.noreply.github.com>
2025-10-08 11:38:22 +0900

    レコードモデルの定義

--- django_bat_sample/django_web/calculator/admin.py
+++ django_bat_sample/django_web/calculator/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Topic # モデルをインポート
+from .models import Topic, Entry
 
 # Register your models here.
 
 admin.site.register(Topic) # 管理サイトでTopicモデルを操作可能にする
+admin.site.register(Entry) # 管理サイトでEntryモデルを操作可能にする
--- django_bat_sample/django_web/calculator/models.py
+++ django_bat_sample/django_web/calculator/models.py
@@ -9,3 +9,20 @@ class Topic(models.Model):
     def __str__(self):
         "モデルの文字列表現を返す"
         return self.text
+
+class Entry(models.Model):
+    """トピックに関して学んだ具体的なこと"""
+    # ForeignKey(外部キー)のインスタンス
+    # on_delete=models.CASCADE) Topicを削除すると連鎖してEntryも削除する
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # Metaクラスはモデルを管理するための追加情報を保持する
+    # 今回は、複数形を「Entrys」ではなく「Entries」を使うよう指示
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """モデルの文字列型表現を返す"""
+        return  f"{self.text[:50]}..."
