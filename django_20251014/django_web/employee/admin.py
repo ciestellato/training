845160e98acd9ad15f50b303589aeec7890d9827
ciestellato <96916847+ciestellato@users.noreply.github.com>
2025-10-16 11:26:07 +0900

    admin

--- django_20251014/django_web/employee/admin.py
+++ django_20251014/django_web/employee/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dept, Emp
 
 # Register your models here.
+
+admin.site.register(Dept)
+admin.site.register(Emp)
--- django_20251014/django_web/employee/models.py
+++ django_20251014/django_web/employee/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# 部署情報を格納するモデルクラス
+class Dept(models.Model):
+    name = models.CharField(max_length=100, verbose_name='部署名')  # 部署名フィールド
+
+    def __str__(self):
+        return self.name  # 管理画面などで部署名を表示
+
+# 従業員情報を格納するモデルクラス
+class Emp(models.Model):
+    name = models.CharField(max_length=100, verbose_name='氏名')  # 従業員の名前
+    age = models.IntegerField(verbose_name='年齢')  # 年齢
+    # ForeignKeyでDeptモデルと関連付け（1対多）。部署が削除されたら従業員も削除される。
+    dept = models.ForeignKey(Dept, on_delete=models.CASCADE, verbose_name='所属部署')  
+
+    def __str__(self):
+        return f"{self.name}（{self.dept.name}）"  # 管理画面などで氏名と部署名を表示
