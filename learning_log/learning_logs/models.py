5a5422bd109d203b94feb9cbca0572b354c9fa57
ciestellato <96916847+ciestellato@users.noreply.github.com>
2025-10-21 14:15:10 +0900

    アプリケーションの作成

---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Topic(models.Model):
+    """"ユーザーが学んでいるトピックを表す"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # 管理画面でレコードで表示される文字列
+    def __str__(self):
+        """モデルの文字列表現を返す"""
+        return self.text
+
+class Entry(models.Model):
+    """トピックに関して学んだ具体的なこと"""
+    # 外部キーで紐づけて、親トピックが削除されたら子エントリーも削除されるように設定
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """モデルの文字列表現を返す"""
+        return f"{self.text[:50]}..."
