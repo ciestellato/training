6cd2fc1a85b0ebd41f1ee2f335bd71f91411dc90
ciestellato <96916847+ciestellato@users.noreply.github.com>
2025-10-22 13:06:31 +0900

    演習問題：一覧・詳細ページ追加

--- pizzeria/pizzas/urls.py
+++ pizzeria/pizzas/urls.py
@@ -7,4 +7,8 @@ app_name = 'pizzas'
 urlpatterns = [
     # ホームページ
     path('', views.index, name='index'),
+    # ピザ一覧
+    path('pizzas/', views.pizzas, name='pizzas'),
+    # ピザのトッピング
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
