c3a965a479b6fc328765255331f4ee706184dea9
ciestellato <96916847+ciestellato@users.noreply.github.com>
2025-10-23 15:18:07 +0900

    ユーザーごとの記事の保護

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """"ユーザーが学んでいるトピックを表す"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # 管理画面でレコードで表示される文字列
     def __str__(self):
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -12,7 +13,7 @@ def index(request):
 @login_required
 def topics(request):
     """すべてのトピックを表示する"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -20,6 +21,11 @@ def topics(request):
 def topic(request, topic_id):
     """1つのトピックとそれについれのすべての記事を表示"""
     topic = Topic.objects.get(id=topic_id)
+
+    # トピックが現在のユーザーが所持するものであることを確認する
+    if topic.owner != request.user:
+        raise Http404
+    
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -34,7 +40,9 @@ def new_topic(request):
         # POSTでデータが送信されたのでこれを処理する
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     
     # 空または無効のフォームを表示する
@@ -67,6 +75,9 @@ def edit_entry(request, entry_id):
     """既存の記事を編集する"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    # トピックが現在のユーザーが所持するものであることを確認する
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # 初回リクエスト時は現在の記事の内容がフォームに埋め込まれている
