b930851a149d0dd271c47753c03945a770c5213e
ciestellato <96916847+ciestellato@users.noreply.github.com>
2025-10-29 10:45:49 +0900

    Bootstrap5の適用

--- learning_log/ll_project/settings.py
+++ learning_log/ll_project/settings.py
@@ -31,14 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 自分のアプリケーション
+    'learning_logs',
+    'accounts',
+
+    # サードパーティのアプリケーション
+    'django_bootstrap5',
+
+    # デフォルトのdjangoアプリケーション
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learning_logs',
-    'accounts',
 ]
 
 MIDDLEWARE = [
